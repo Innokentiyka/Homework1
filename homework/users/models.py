5745de0c5c6a107bda390565a0eff875ebcea0a8
PC <kerik006@mail.ru>
2024-03-03 13:53:48 +0300

    hhh

---
+++ homework/users/models.py
@@ -0,0 +1,12 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=20)
+    city = models.CharField(max_length=100)
+    avatar = models.ImageField(upload_to='avatars/')
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
